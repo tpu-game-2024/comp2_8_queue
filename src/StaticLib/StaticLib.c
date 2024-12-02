d38f10e56b7f10e3440f5f8c07a6e9c2adcfcf08
JcdbTPU40 <g2427040@st.t-kougei.ac.jp>
2024-12-02 15:48:51 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,38 +34,66 @@ void finalize(QUEUE* q)
 // valの値をキューに入れる。実行の成否を返す
 bool enqueue(QUEUE* q, int val)
 {
-	// ToDo: valのデータをキューに追加します
-	// 上手くいかない場合にはfalseを返します
-	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
+	if (q == NULL || q->memory_begin == NULL) return false;
 
-	return false;
+	if (countQueueableElements(q) <= 0) return false;
+
+	*(q->tail) = val;
+
+	q->tail++;
+	if (q->tail == q->memory_end) {
+		q->tail = q->memory_begin;
+	}
+
+	return true;
 }
 
 
 // addrから始まるnum個の整数をキューに入れる。実行の成否を返す
 bool enqueue_array(QUEUE* q, int* addr, int num)
 {
-	// ToDo: addrからnum個のデータをキューに追加します
-	// 上手くいかない場合にはfalseを返します
-	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
+	if (q == NULL || q->memory_begin == NULL || addr == NULL || num <= 0) return false;
 
-	return false;
+	if (countQueueableElements(q) < num) return false;
+
+	for (int i = 0; i < num; i++) {
+		if (!enqueue(q, addr[i])) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 // キューから一つの要素を取り出す(不具合時は0を返す)
 int dequeue(QUEUE* q)
 {
-	// ToDo: 先頭のデータを返します
+	if (q == NULL || q->memory_begin == NULL || isEmpty(q)) return 0;
 
-	return 0;
+	int value = *(q->head);
+
+	q->head++;
+	if (q->head == q->memory_end) {
+		q->head = q->memory_begin;
+	}
+
+	return value;
 }
 
 // addrにキューからnumの要素を取り出す。取り出せた個数を返す
 int dequeue_array(QUEUE* q, int* addr, int num)
 {
-	// ToDo: 先頭からnum個のデータをaddrに格納します
+	if (q == NULL || q->memory_begin == NULL || addr == NULL || num <= 0) return 0;
 
-	return 0;
+	int count = 0;
+
+	for (int i = 0; i < num; i++) {
+		if (isEmpty(q)) break;
+		addr[i] = dequeue(q);
+		count++;
+	}
+
+	return count;
 }
 
 // キューが空かどうかを調べる
@@ -89,7 +117,7 @@ int countQueuedElements(const QUEUE* q)
 	if (q == NULL || q->memory_begin == NULL) return 0;
 
 	int max_counts = getMaxCount(q);
-	return (q->head + max_counts - q->tail) % max_counts;
+	return (q->tail >= q->head) ? (q->tail - q->head) : (max_counts - (q->head - q->tail));
 }
 
 // 挿入可能なデータ数を得る
